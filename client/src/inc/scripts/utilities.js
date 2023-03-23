8a4f3bdba06de45891db934a49c3e8228fd9fdbc
geohanz <ngmhah@gmail.com>
2023-03-23 17:19:26 +0700

    update homepage

---
+++ client/src/inc/scripts/utilities.js
@@ -0,0 +1,15 @@
+import jQuery from "jquery";
+
+export function navigate(page) {
+	window.location.href = `${page}`;
+}
+
+const closeUtilityComponent = () => {
+	jQuery(($) => {
+		$.noConflict();
+
+		$(".utility-component").removeClass("add-utility-component-height");
+	});
+};
+
+export default navigate;
--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const About = () => {
     return (
         <React.Fragment>
-            <div>test123</div>
+            
         </React.Fragment>
     );
 };
